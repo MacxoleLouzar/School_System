9b5fa3e85fbf3f3c68dae638a2885401a4835eec
Masixole Mtsikini <macxolelouzaar@gmail.com>
2022-06-17 15:49:36 +0200

    Added appsettings

--- SchoolSystem/SchoolSystem.Persistence/SchoolSystemContext.cs
+++ SchoolSystem/SchoolSystem.Persistence/SchoolSystemContext.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SchoolSystem.Models.Entities;
 
 namespace SchoolSystem.Persistence
 {
    public class SchoolSystemContext : DbContext
     {
-        
+        public SchoolSystemContext(DbContextOptions<SchoolSystemContext>opt)
+                                 :base(opt)
+        {
+
+        }
+        public DbSet<User> users{get;set;}
     }
 }
