18346567e1471c2aa2bf53d5a849e78a81ea6f93
Masixole Mtsikini <macxolelouzaar@gmail.com>
2022-06-17 12:48:12 +0200

    add DBContext

---
+++ SchoolSystem/SchoolSystem.Persistence/SchoolSystemContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SchoolSystem.Persistence
+{
+   public class SchoolSystemContext : DbContext
+    {
+        
+    }
+}
