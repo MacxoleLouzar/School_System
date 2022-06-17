28f0bd993da73dbf748ddc825153c212a6207679
ZB <timeanalyser@code.web.za>
2022-06-17 11:25:59 +0200

    Add first entities and persistence project

---
+++ SchoolSystem/SchoolSystem.Models/Entities/EntityBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSystem.Domain.Entities;
+
+public abstract class EntityBase
+{
+    protected EntityBase() { }
+
+    public EntityBase(DateTime? createdDate)
+    {
+        CreatedDateTime = createdDate ?? throw new ArgumentNullException(nameof(createdDate));
+    }
+
+    public int Id { get; }
+
+    public DateTime CreatedDateTime { get; } = default!;
+
+    public DateTime ModifiedDateTime { get; } = default!;
+
+    //TODO: Add Created and Modified UserId after creating User entity
+
+}
+
+
+
---
+++ SchoolSystem/SchoolSystem.Persistence/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace SchoolSystem.Persistence
+{
+    public class Class1
+    {
+
+    }
+}
